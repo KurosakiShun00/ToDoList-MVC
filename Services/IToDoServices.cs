08a7eed2643f5509868155bbcdcb6b4c8606b978
KurosakiShun00 <gio.arru@yahoo.it>
2026-05-20 14:55:25 +0200

    Develop-Features : Aggiunti interfaccia e implementazione dei servizi

---
+++ Services/IToDoServices.cs
@@ -0,0 +1,16 @@
+﻿using ToDoList_MVC.Models;
+
+namespace ToDoList_MVC.Services
+{
+    public interface IToDoServices
+    {
+
+        Task<IEnumerable<ToDo>> GetAllAsync();
+        Task<IEnumerable<ToDo>> GetCompleteAsync();
+        Task<ToDo?> GetByIdAsync(int id);
+        Task<ToDo> CreateAsync(ToDoDTO to_do_dto);
+        Task<bool> UpdateAsync(int id, ToDoDTO to_do_dto);
+        Task<bool> DeleteAsync(int id);
+
+    }
+}
