9f5e0e62da81562e77ed7662a7c6676b4181fadb
Giovanni Arru <gio.arru@yahoo.it>
2026-07-30 16:07:27 +0200

    Develop.Auth FEAT: implementata autenticazione con Identity e pagine di login/registrazione

--- Data/ToDoDB.cs
+++ Data/ToDoDB.cs
@@ -1,13 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using ToDoList_MVC.Models;
 
 namespace ToDoList_MVC.Data
 {
-    public class ToDoDB : DbContext
-    {
-        public ToDoDB(DbContextOptions<ToDoDB> options) : base(options) { }
+    public class ToDoDB : IdentityDbContext
 
-        public DbSet<ToDo>  ToDos => Set<ToDo>();
-        public DbSet<ToDoList> ToDosLists => Set<ToDoList>();
+    {
+    public ToDoDB(DbContextOptions<ToDoDB> options) : base(options)
+    {
+    }
+
+    public DbSet<ToDo> ToDos => Set<ToDo>();
+    public DbSet<ToDoList> ToDosLists => Set<ToDoList>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<ToDoList>()
+            .HasOne(t => t.User)
+            .WithMany()
+            .HasForeignKey(t => t.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
     }
 }
--- Models/ToDoList.cs
+++ Models/ToDoList.cs
@@ -1,10 +1,20 @@
-﻿namespace ToDoList_MVC.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoList_MVC.Models
 {
     public class ToDoList
     { 
         public int Id { get; set; }
         public string? Name { get; set; }
 
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(UserId))]
+        public virtual IdentityUser? User { get; set; }
+        
         public List<ToDo> ToDos { get; set; } = new();
     }
 }
