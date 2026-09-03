9699c522efe81892cf483b91b5982ebc7d9c72af
Giovanni Arru <gio.arru@yahoo.it>
2026-09-03 16:14:25 +0200

    Develop.Views FIX: update grafico di alcune viste e settata path di env sviluppo per le mail

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -7,14 +7,30 @@ namespace ToDoList_MVC.Services;
 public class EmailSender:IEmailSender
 {
     private readonly IConfiguration _configuration;
+    private readonly ILogger<EmailSender> _logger;
+    private readonly IWebHostEnvironment _env;
     
-    public EmailSender(IConfiguration configuration)
+    public EmailSender(IConfiguration configuration, ILogger<EmailSender> logger, IWebHostEnvironment env)
     {
         _configuration = configuration;
+        _logger = logger;
+        _env = env;
     }
 
     public async Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
+        if (_env.IsDevelopment() || string.IsNullOrEmpty(_configuration["Smtp:Host"]))
+        {
+            _logger.LogInformation("================ MOCK EMAIL GENERATA ================");
+            _logger.LogInformation("A: {Email}", email);
+            _logger.LogInformation("Oggetto: {Subject}", subject);
+            _logger.LogInformation("Messaggio HTML:\n{Body}", htmlMessage);
+            _logger.LogInformation("====================================================");
+            
+            await Task.CompletedTask;
+            return;
+        }
+        
         var host = _configuration["Smtp:Host"];
         var port = int.Parse(_configuration["Smtp:Port"] ?? "587");
         var username = _configuration["Smtp:Username"];
