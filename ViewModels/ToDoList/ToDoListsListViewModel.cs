be1860a81d3fe64e68627e0ce348ae04193f653a
Giovanni Arru <gio.arru@yahoo.it>
2026-09-04 11:56:43 +0200

    Develop.Views FIX : minor fixes

--- ViewModels/ToDoList/ToDoListsListViewModel.cs
+++ ViewModels/ToDoList/ToDoListsListViewModel.cs
@@ -7,6 +7,7 @@ public class ToDoListsListViewModel
     public int Id { get; set; }
     public string? Name { get; set; }
     public int Remaining => ToDos.Count(x => !x.IsCompleted);
-    public bool IsFinished => Remaining == 0;
+    public bool IsFinished => Remaining == 0 && ToDos.Count != 0;
+    public int ExpiredCount => ToDos.Count(x => x.Deadline<DateTime.Now);
     public List<ToDoDTO> ToDos { get; set; } = new();
 }
