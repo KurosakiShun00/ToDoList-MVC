91ebb6e545f66e7f73853d97d5ab9d2065e08a40
Giovanni Arru <gio.arru@yahoo.it>
2026-09-04 11:23:40 +0200

    Develop small fix

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -173,7 +173,6 @@ namespace ToDoList_MVC.Areas.Identity.Pages.Account
                 }
             }
 
-            // If we got this far, something failed, redisplay form
             return Page();
         }
 
